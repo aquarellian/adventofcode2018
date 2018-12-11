081b929244bf7987a368426b346843dbb788ee63
Tatiana Didik <tatianadidik@potatocat.fios-router.home>
2018-12-10 20:04:30 -0500

    Task 20

--- src/task20.py
+++ src/task20.py
@@ -8,7 +8,7 @@ def substr_between(line, start_mrk, end_mrk):
     return line[start:end]
 
 
-with open("../resources/task20.txt") as f:
+with open("../resources/task20.test.txt") as f:
     content = f.readlines()
     data = {}
     minx = None
@@ -22,7 +22,7 @@ with open("../resources/task20.txt") as f:
         velocity = substr_between(line, 'velocity=<', '>').split(',')
         dx = int(velocity[0])
         dy = int(velocity[1])
-        data[x, y] = [dx, dy]
+        data[(x, y)] = [dx, dy]
         print(str(x) + ' ' + str(y) + ' ' + str(dx) + ' ' + str(dy))
         minx = x if minx is None or minx > x else minx
         maxx = x if maxx is None or maxx < x else maxx
@@ -33,49 +33,35 @@ with open("../resources/task20.txt") as f:
     print('maxy=' + str(maxy))
 
     keep_going = True
-    import copy
-    data_before = copy.copy(data)
-    data_after = {}
     sec = 0
     deltax = abs(minx) if minx < 0 else -minx
     deltay = abs(miny) if miny < 0 else -miny
 
 
     while keep_going:
-        print('after ' + str(sec) + ' sec')
-        import cv2
-        import numpy as np
-        img = np.zeros((maxx-minx, maxy-miny, 2), np.uint8)
+        updated = []
+        for coord, vel in data.items():
+            print(coord)
+            print(vel)
+            x=coord[0]
+            y=coord[1]
+            dx = vel[0]
+            dy = vel[1]
+            newx = x+sec*dx
+            newy = y+sec*dy
+            updated.append([newx, newy])
+        print(updated)
 
-        # array = np.zeros([maxx-minx, maxy-miny, 3], dtype=np.uint8)
-        # with open('../resources/task20.out.txt', 'w', newline='') as r:
+        print('after ' + str(sec) + ' sec')
         for y in range(miny, maxy+1):
             printlane=''
             for x in range(minx, maxx+1):
-                vel = data_before.get((x, y), None)
-                if vel is not None:
-                    printlane += '#'
-                    dx = vel[0]
-                    dy = vel[1]
-                    newx = x+dx
-                    newy = y+dy
-                    data_after[newx, newy] = vel
+                printlane += ( '#' if [x,y] in updated else '.')
 
-                    img.itemset((x+deltax, y+deltay, 2), 255)
-                else:
-                    printlane += '.'
-
-        # r.write(printlane + '\n')
-
-            # print(printlane)
-        from PIL import Image
-        # img = Image.fromarray(array)
-        img.save('../resources/task20.out.png')
+            print(printlane)
 
         keep_going = input('continue?') != 'n'
 
-        data_before = copy.copy(data_after)
-        data_after = {}
         sec +=1
 
 
