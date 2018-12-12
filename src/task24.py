9099c1dedeb3b603bcd910982b6a6b59458e1f16
Tatiana Didik <Tatiana.Didik@thomsonreuters.com>
2018-12-12 17:05:54 -0500

    Day 12

--- src/task24.py
+++ src/task24.py
@@ -2,7 +2,7 @@ with open("../resources/task23.txt") as f:
     content = f.readlines()
     plants = ''
     rules = {}
-    generations = 20
+    generations = 102 ## this is a magic number after which pattern doesn't change, just move 1 step to right
 
     for line in content:
         if 'initial state: ' in line:
@@ -20,7 +20,7 @@ with open("../resources/task23.txt") as f:
     # print(plants)
     nextgen = ''
     all = set()
-
+    pattern_starts = None
     for i in range(1, generations+1):
         for j in range(0, len(plants)):
             if j == 0:
@@ -38,9 +38,29 @@ with open("../resources/task23.txt") as f:
         plants = nextgen
         nextgen = ''
 
+        stripped = plants.strip('.')
+        if stripped not in all:
+            all.add(stripped)
+        else:
+            pattern_starts = i
+            break
+
     res = 0
+    count = 0
     for i in range(0, len(plants)):
         if plants[i] == '#':
-            res+=(i-generations)
+            res += (i-generations)
+            count += 1
+
+    print(count)
     print(res)
+
+    target = 50000000000
+    diff = count * (target-pattern_starts)
+    print(diff)
+
+    print(res + diff)
+
+    # answer 2950000001598
+
 
