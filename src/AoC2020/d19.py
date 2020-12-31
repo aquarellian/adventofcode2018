57c80237dd3485c063eb368a09d1c819447aa917
Tatiana Didik <tatiana.didik@bjss.com>
2020-12-31 17:12:13 -0500

    d19

--- src/AoC2020/d19.py
+++ src/AoC2020/d19.py
@@ -1,9 +1,12 @@
 def split_rule(s):
     if '((' in s:
         part1 = s[:s.index('((')]
-        part2 = s[s.index('(('):s.index('))') +2]
-        part3 = s[s.index('))') +2:]
-        return split_rule(part1) + part2 + split_rule(part3)
+        part2 = s[s.index('(('):s.index(')+)') +3]
+        part3 = s[s.index(')+)') +3:]
+        arr = split_rule(part1) + [part2] + split_rule(part3)
+        while '' in arr:
+            arr.remove('')
+        return arr
     else:
         return s.split('|')
 
@@ -15,13 +18,20 @@ def calc(rules, key, depth, maxdepth):
         return rules[key]
     if key == 8:
         v = calc(rules, 42, depth + 1, maxdepth).replace('"', '')
-        rules[8] = '"((' + v + ')*)"'
+        rules[8] = '"((' + v + ')+)"'
         return rules[8]
     elif key == 11:
         v1 = calc(rules, 42, depth + 1, maxdepth).replace('"', '')
         v2 = calc(rules, 31, depth + 1, maxdepth).replace('"', '')
-        rules[11] = '"((' + v1 + ')*(' + v2 + '))*"'
+        rules[11] = '"((' + v1 + '){1}(' + v2 + '){1}) | "' + '"((' + v1 + '){2}(' + v2 + '){2}) | "' + \
+        '"((' + v1 + '){3}(' + v2 + '){3}) | "' + '"((' + v1 + '){4}(' + v2 + '){4}) | "' + \
+        '"((' + v1 + '){5}(' + v2 + '){5}) | "' + '"((' + v1 + '){6}(' + v2 + '){6})"'
         return rules[11]
+    elif key == 0:
+        v1 = calc(rules, 8, depth + 1, maxdepth).replace('"', '')
+        v2 = calc(rules, 11, depth + 1, maxdepth).replace('"', '')
+        rules[0] = '(' + v1 +')(' + v2 + ')'
+        return rules[0]
     elif ' | ' in rules[key]:
         keyrules = split_rule(rules[key])
     else:
@@ -34,11 +44,11 @@ def calc(rules, key, depth, maxdepth):
         for r in rule:
             v = calc(rules, int(r), depth + 1, maxdepth).replace('"', '')
             if len(s) == 0:
-                s = set([x.strip() for x in split_rule(rules[key])])
+                s = set([x.strip() for x in split_rule(v)])
             else:
                 s = set([x.strip() + y.strip()
                          for x in s if len(x) < maxdepth
-                         for y in split_rule(rules[key]) if len(y) < maxdepth])
+                         for y in split_rule(v) if len(y) < maxdepth])
         arr += list(s)
     rules[key] = " | ".join(['"' + s + '"' for s in set(arr)])
     return rules[key]
@@ -50,11 +60,11 @@ import re
 def check(rule, line):
     # options = [x.replace('"', '') for x in rule.split(" | ")]
     # return line in options
-    pattern = "^" + rule.replace(" ", "") + "$"
+    pattern = "^(" + rule.replace('"', '').replace(" ", "") + ")$"
     return re.match(pattern, line) is not None
 
 
-with open("/Users/tatianadidik/IdeaProjects/adventofcode2018/resources/AoC2020/d19.txt") as f:
+with open("/Users/tatianadidik/IdeaProjects/adventofcode2018/resources/AoC2020/d19_2.txt") as f:
     content = f.readlines()
     rules = dict()
 
@@ -71,7 +81,11 @@ with open("/Users/tatianadidik/IdeaProjects/adventofcode2018/resources/AoC2020/d
     rule = calc(rules, 0, 0, max_length)
     options = set([x.replace('"', '') for x in rule.split(" | ")])
     for line in lines:
-        res += int(line.strip() in options)
+        res += check(rule, line.strip())
+        maxlen = max(maxlen, len(line.strip()))
+        # res += int(line.strip() in options)
     print(res)
 
+#441 high
+#391 high
     # year = "^[0-9]{4}$"
